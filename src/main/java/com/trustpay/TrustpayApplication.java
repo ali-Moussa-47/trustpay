99c5aefbea2bfd4f9ccb317bccb02605d94c5e8f
moussa <ali.moussa.1@ulaval.ca>
2025-11-10 21:14:02 -0500

    Initial commit - TrustPay Spring Boot project

---
+++ src/main/java/com/trustpay/TrustpayApplication.java
@@ -0,0 +1,13 @@
+package com.trustpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trustpay/TrustpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
